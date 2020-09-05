69ae83dc645eafc92ae2910bc70ce8549fc2ff28
geekymeeky <66238394+geekymeeky@users.noreply.github.com>
2020-09-05 17:12:41 +0530

    Update models.py

--- exams/models.py
+++ exams/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=50)
     dept_code = models.CharField(max_length=5)
-    year = models.IntegerField(max_length=4)
-    roll_no = models.IntegerField(max_length=12)
-    registration_no = models.IntegerField(max_length=20)
-    mobile_no = models.IntegerField(max_length=10)
+    year = models.IntegerField()
+    roll_no = models.IntegerField(primary_key=True)
+    registration_no = models.IntegerField()
+    mobile_no = models.IntegerField()
 
 
 class Score(models.Model):
-    roll_no = models.IntegerField(max_length=12)
+    roll_no = models.IntegerField()
     subject_code = models.CharField(max_length=6)
     credit_score = models.FloatField()
 
