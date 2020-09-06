511fbe5e2ac2d465270b6212bd9c185ac62f7b5c
avinashkranjan <ranjan.avinash@hotmail.com>
2020-09-06 09:08:33 +0530

    cleanup

--- cgs/asgi.py
+++ cgs/asgi.py
@@ -1,16 +1,7 @@
-"""
-ASGI config for online_class project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_class.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cgs.settings')
 
 application = get_asgi_application()
--- class/views.py
+++ class/views.py
@@ -1,43 +1,25 @@
 from django.shortcuts import render
 import datetime
-import calendar 
+import calendar
 
 from .models import (
-    Class, 
+    Class,
     Routine,
 )
 
+
 def class_view(request):
     day = datetime.datetime.today().isoweekday()
-    
+
     if day < 7:
         day = day + 1
     else:
         day = day - 6
 
-    routines = Routine.objects.filter(class_name__sem = request.user.student.sem)
+    routines = Routine.objects.filter(class_name__sem=request.user.student.sem)
     context = {
         'routines': routines,
         'todaysDay': day
     }
 
     return render(request, 'students/class_home.html', context)
-
-
-# def  add_resources(request):
-#     if request.user.is_authenticated:
-#         if request.user.is_teacher:
-#             if request.method == 'POST':
-#                 form = classAddForm(request.POST, request.FILES)
-#                 if form.is_valid():
-#                     classes = form.save(commit=False)
-#                     classes.teacher_name = request.user
-#                     classes.save()
-#                     return redirect('class_page')
-#             else:
-#                 form = ExamQuestionAddForm()
-    
-#             return render(request, 'teachers/class_add.html', {'form':form})
-#         else:
-#             return redirect('class_page')
-#     return redirect('login')
